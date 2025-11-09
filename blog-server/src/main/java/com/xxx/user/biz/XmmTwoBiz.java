dd206c65580ce0c5d0f3f558d2199762a5b13f4b
xxx <486474793@qq.com>
2025-11-09 17:09:30 +0800

    "airate=0&aitool=tongyi&新增XmmBiz层2222"

--- blog-server/src/main/java/com/xxx/user/biz/XmmTwoBiz.java
+++ blog-server/src/main/java/com/xxx/user/biz/XmmTwoBiz.java
@@ -3,5 +3,6 @@ package com.xxx.user.biz;
 public class XmmTwoBiz {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!2222");
     }
 }
